26b9f0ee1c2f705956b20acdacd89d4efe7684e1
Stan Iwan <stan.iwan@sofomo.com>
2024-06-20 08:30:12 -0500

    Add reader to STFWriter base test - WIP

--- tests/stf_writer_test/main.cpp
+++ tests/stf_writer_test/main.cpp
@@ -1,7 +1,15 @@
 #include "stf-inc/stf_writer.hpp"
+#include "stf-inc/stf_reader.hpp"
 #include "stf-inc/stf_record_types.hpp"
 
-int main() {
+#include "gtest/gtest.h"
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+
+TEST(STFWriterTest, STFWriterBasicTest) {
     uint64_t pc = 0x1000;
 
     stf::STFWriter stf_writer;
@@ -19,5 +27,28 @@ int main() {
     stf_writer << stf::InstOpcode32Record(0x00b60733);
     
     stf_writer.close();
-    return 0;
+
+    stf::STFReader stf_reader;
+    stf_reader.open("stf_write_test.zstf");
+
+    bool found_trace_info = false;
+    bool found_opcode = false;
+
+    for(const auto& record : stf_reader) {
+        if (auto trace_info = dynamic_cast<const stf::TraceInfoRecord*>(&record)) {
+            EXPECT_EQ(trace_info->getTraceVersion(), 1);
+            EXPECT_EQ(trace_info->getISA(), 2);
+            EXPECT_EQ(trace_info->getReserved(), 0);
+            EXPECT_STREQ(trace_info->getDescription().c_str(), "Trace from Dromajo");
+            found_trace_info = true;
+        } else if (auto opcode_record = dynamic_cast<const stf::InstOpcode32Record*>(&record)) {
+            EXPECT_EQ(opcode_record->getOpcode(), 0x00b60733);
+            found_opcode = true;
+        }
+    }
+
+    stf_reader.close();
+
+    EXPECT_TRUE(found_trace_info);
+    EXPECT_TRUE(found_opcode);
 }
